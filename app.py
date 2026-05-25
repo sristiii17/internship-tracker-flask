56519d4eedc45b85c480a60d0d6f1e45b6d3bafb
Sristi Sharma <sristis281@gmail.com>
2026-05-25 18:31:04 +0530

    Updated internship tracker

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def edit(index):
     return render_template("edit.html", app=applications[index], index=index)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
